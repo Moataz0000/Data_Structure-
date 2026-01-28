a956cde64b887dfcee3e6315f170e2eafa3c89b3
Moataz0000 <moatazfawzy150@gmail.com>
2026-01-28 21:42:45 +0200

    feat: adding traverse method and find lowest value

--- linked_list.py
+++ linked_list.py
@@ -11,20 +11,39 @@ class Node:
         self.data = data
         self.next = None
 
+def traverse(head):
+    """Go through the linked list by following the links from one node to the next"""
+    current_node = head
+    while current_node:
+        print(current_node.data, end=" -> ")
+        current_node = current_node.next
+    print(None)
+
+
+def find_lowest_value(head):
+    min_value = head.data
+    current_node = head.next
+
+    while current_node:
+        if current_node.data < min_value:
+            min_value = current_node.data
+        current_node = current_node.next 
+    return min_value
+
+
+
 
 
 
 node1 = Node(1)
 node2 = Node(2)
 node3 = Node(3)
+node4 = Node(0)
 
 node1.next = node2
 node2.next = node3
+node3.next = node4
 
 
-current_node = node1
-
-while current_node:
-    print(current_node.data, end=" -> ")
-    current_node = current_node.next
-print("null")
+traverse(node1)
+print(f'The lowest value in the linked list is: {find_lowest_value(node1)}')
